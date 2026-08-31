c3afe911d51b1a94f96ddd70e48ac83712c2362a
Morten Bjørn Hallkvist <dev@mortenbjoern.com>
2026-08-31 16:53:10 +0300

    new order of images for splash

--- src/_data/splashImages.js
+++ src/_data/splashImages.js
@@ -16,6 +16,26 @@ module.exports = [
 		showOnMobile: true,
 	},
 	{
+		src: "/assets/images/splash-9.jpeg",
+		alt: "Rebuild community building connections",
+		showOnMobile: true,
+	},
+	{
+		src: "/assets/images/splash-11.jpeg",
+		alt: "Collaborative innovation at Rebuild",
+		showOnMobile: true,
+	},
+	{
+		src: "/assets/images/splash-13.jpeg",
+		alt: "More collaborative innovation at Rebuild",
+		showOnMobile: true,
+	},
+	{
+		src: "/assets/images/splash-0.jpg",
+		alt: "Rebuild gathering participants collaborating",
+		showOnMobile: true,
+	},
+	{
 		src: "/assets/images/splash-8.jpeg",
 		alt: "European social platforms ecosystem",
 		showOnMobile: true,
